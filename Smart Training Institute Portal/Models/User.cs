0251c92626e66647c7ed625f5a7fcab5eca21a7a
Amin Al-Momani <amin2002.02@gmail.com>
2026-05-28 19:42:43 +0300

    fixed User Model

--- Smart Training Institute Portal/Models/User.cs
+++ Smart Training Institute Portal/Models/User.cs
@@ -4,7 +4,7 @@ namespace Smart_Training_Institute_Portal.Models
 {
 	public class User : IdentityUser
 	{
-		public string FullName { get; set; }
+		public string FullName { get; set; } = string.Empty;
 		public StudentProfile? StudentProfile { get; set; }
 		public InstructorProfile? InstructorProfile { get; set; }
 		public ICollection<GradeLog> GradeLogs { get; set; } = new List<GradeLog>();
